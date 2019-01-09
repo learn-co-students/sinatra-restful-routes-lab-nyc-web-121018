bab8380831688b73e25d8fb83c36ff0aa807ed31
Oli Nelson <olivernelson@icloud.com>
2019-01-09 14:12:46 -0500

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,46 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+get '/recipes' do
+  erb :recipes
+end
+
+
+get '/recipes/new' do
+  erb :new_recipe
+end
+
+get '/recipes/:id' do
+  @recipe = Recipe.find(params[:id])
+  erb :show
+end
+
+get '/recipes/:id/edit' do
+  @recipe = Recipe.find(params[:id])
+  erb :edit_recipe
+end
+
+delete '/recipes/:id' do
+  Recipe.destroy(params[:id])
+  redirect to "/recipes"
+end
+
+patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+
+    redirect to "/recipes/#{ @recipe.id }"
+  end
+
+
+
+post '/recipes' do
+  params.to_s
+  @recipe = Recipe.create(params)
+  redirect to "/recipes/#{@recipe.id}"
+end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,4 @@
 #Placeholder for a model
+class Recipe < ActiveRecord::Base
+  
+end #end of class
